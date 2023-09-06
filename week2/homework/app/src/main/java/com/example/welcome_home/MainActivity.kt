3c903ebc80a06cc8a2decd281354344fa76af3ec
quyendv <daovanquyen45@gmail.com>
2023-09-07 06:54:42 +0700

    week2-homework: simple layout
    toolbar, radio, number picker

---
+++ week2/homework/app/src/main/java/com/example/welcome_home/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.welcome_home
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.NumberPicker
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Donation.1.5"
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
+
+        val numberPicker = findViewById<NumberPicker>(R.id.number_picker)
+        numberPicker.minValue = 0
+        numberPicker.maxValue = 1000
+    }
+}
