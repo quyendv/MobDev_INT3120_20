f74a966259d5242332da8e5d02d9ace9ac7bb839
quyendv <daovanquyen45@gmail.com>
2023-09-07 07:54:45 +0700

    week2-homework: complete element

--- week2/homework/app/src/main/java/com/example/welcome_home/MainActivity.kt
+++ week2/homework/app/src/main/java/com/example/welcome_home/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import android.widget.ProgressBar
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +22,8 @@ class MainActivity : AppCompatActivity() {
         val numberPicker = findViewById<NumberPicker>(R.id.number_picker)
         numberPicker.minValue = 0
         numberPicker.maxValue = 1000
+
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        progressBar.progress = 30
     }
 }
