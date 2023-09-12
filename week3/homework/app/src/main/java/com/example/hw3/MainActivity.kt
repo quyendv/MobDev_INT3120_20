8fff716d7292f2ec0034c63840adf3ec0c41655c
quyendv <daovanquyen45@gmail.com>
2023-09-12 22:52:42 +0700

    wee3-homework: example layouts

---
+++ week3/homework/app/src/main/java/com/example/hw3/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hw3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.linear_layout)
+//        setContentView(R.layout.relative_layout)
+//        setContentView(R.layout.absolute_layout)
+//        setContentView(R.layout.constraint_layout)
+//        setContentView(R.layout.table_layout)
+        setContentView(R.layout.frame_layout)
+    }
+}
